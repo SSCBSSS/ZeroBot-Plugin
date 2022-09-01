e50d634ae52e50288515014d995cec506132f3cc
SSCBSSS <112654899+SSCBSSS@users.noreply.github.com>
2022-09-02 02:44:07 +0800

    yes

--- main.go
+++ main.go
@@ -204,8 +204,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 12345678)
+	 sus = append(sus, 87654321)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
